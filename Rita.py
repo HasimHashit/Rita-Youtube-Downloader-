0058525c3b3a91fd58566e53cc835a9a4046b5a8
TheHoddieWarrior <59323060+TheHoddieWarrior@users.noreply.github.com>
2020-02-09 06:36:45 -0800

    added a playlist downloader
    now you can download YouTube playlist and cleaned up the code duo lacks a few things
    #LACKS
    -Download audio playlist
    - multi-threading 
    -GUI
    
    I'LLL WORK ON THEM BUT Y'LL SHOULD PLAY AROUND AND I'LL APPLY YOUR MODIFICATIONS OR JUST USE IT TO BUILD YOURS.....

--- Rita.py
+++ Rita.py
@@ -1,6 +1,13 @@
+import time
 import pafy
+import youtube_dl
+'''
+ REQUIREMENTS
+ - Install pafy
+ - Install Youtube_dl latest update
+'''
 
-def audio():
+def singleAudio():
     ##pafy for getting url==> audio
     #url of the video
     url = input('Enter your url: ')
@@ -18,7 +25,7 @@ def audio():
 
 
 
-def video():
+def singleVideo():
     ##pafy for getting url==> video
     ##url of the video
     url = input('Enter your url: ')
@@ -32,14 +39,40 @@ def video():
     best_quality_audio.download()
 
 
+def playlistVideo():
+    playlistlinks= {}
+    while(1):
+        link = input("Copy and paste the Youtube link: ")
+        youtube_dl.YoutubeDL(playlistlinks).download([link])
+    
+
+
+
+
 def userpick():
-    userinput = eval(input('Enter you pick 1.audio 2.video: '))
-    if userinput == 1:
-        audio()
-    elif userinput == 2:
-        video()  
-    else:
-        print('pls pick a valid number')    
+    print('Please pick a program to run')
+    time.sleep(3)
+    print('1.Single Downlaod\n2.Multi Download')
+    singlePickValue = int(input('Enter Value: '))
+    print('Loading..')
+    time.sleep(2)
+    print('Loading............')
+    time.sleep(3)
+    print('Trying to get to Thanos before he gets to me..........')
+    time.sleep(3)
+    print('"Dread it Run from it, Destiny arrives all the same, Now its here, or should i say I AM" ')
+    
+    if singlePickValue == 1:
+        print('1.Audio\n2.Video')
+        singlePickFinal = int(input('Enter Value:'))
+
+        if singlePickFinal == 1:
+            singleAudio()
+        elif singlePickFinal == 2:
+            singleVideo()    
+        
+    elif singlePickValue == 2:
+        playlistVideo()
 
 
 
